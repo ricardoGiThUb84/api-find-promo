ca9f2dea11867226e9e0909622235673caff8925
ricardo <rosastaricardo84@gmail.com>
2024-08-09 20:44:45 -0300

    properties database config

---
+++ src/main/java/br/com/api/lista_produto/find_promo/FindPromoApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.lista_produto.find_promo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindPromoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindPromoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/lista_produto/find_promo/FindPromoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.lista_produto.find_promo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindPromoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
